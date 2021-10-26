cda8c7d8db60b21f8e8b14d4ba410aebde8a40e0
Alawode Emmanuel <alawodeemmanuel2@gmail.com>
2021-10-26 12:13:51 +0100

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom';
+import App from './main/App';
+import './indexStyle.scss';
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  //-->   set mode
+  mode: 'development',
+
+  //-->   set port for development
+  devServer: {
+    port: 3000,
+  },
+
+  //-->  allow source map
+  devtool: 'source-map',
+
+  //-->  set entry point
+  entry: './src/pages/index.js',
+
+  //-->  set output path
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+
+  //-->  main module setting and loaders
+  module: {
+    rules: [
+      //-->  js loader
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      //-->  css laoder
+      {
+        test: /\.css$/i,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      //-->   sass loader
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      //-->  set svg asset tye
+      {
+        test: /\.svg$/i,
+        type: 'asset',
+      },
+      //--> set type for image an jpeg
+      {
+        test: /\.png|jpe?g|gif/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  //-->  all plugins
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+};
